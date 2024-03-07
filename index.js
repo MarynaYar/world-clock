58edc287e9a27be8ac31d7ea4ac82a43ee90e302
MarynaYar <m.yaroshchuk92@gmail.com>
2024-03-07 13:41:28 +0100

    Updated the time

---
+++ index.js
@@ -0,0 +1,31 @@
+setInterval(function () {
+  // Zurich
+  let zurichElement = document.querySelector("#zurich");
+  let zurichDateElement = zurichElement.querySelector(".date");
+  let zurichTimeElement = zurichElement.querySelector(".time");
+  let zurichZone = moment().tz("Europe/Zurich");
+
+  zurichTimeElement.innerHTML = `${zurichZone.format("hh:mm:ss")} <small>${zurichZone.format("A")}</small>`
+  zurichDateElement.innerHTML = zurichZone.format("dddd, MMMM Do YYYY");
+
+  // London
+  let londonElement = document.querySelector("#london");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonZone = moment().tz("Europe/London");
+
+  londonTimeElement.innerHTML = `${londonZone.format("hh:mm:ss")} <small>${londonZone.format("A")}</small>`
+
+  // Sydney
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyZone = moment().tz("Australia/Sydney");
+
+  sydneyTimeElement.innerHTML = `${sydneyZone.format("hh:mm:ss")} <small>${sydneyZone.format("A")}</small>`
+
+  // New York
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkZone = moment().tz("America/New_York");
+
+  newYorkTimeElement.innerHTML = `${newYorkZone.format("hh:mm:ss")} <small>${newYorkZone.format("A")}</small>`
+}, 1000)
