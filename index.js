849748f130ed05ad589ce27fb5a4ec5e91ca1f43
MarynaYar <m.yaroshchuk92@gmail.com>
2024-03-17 14:31:55 +0100

    updated CSS and JS

--- index.js
+++ index.js
@@ -1,82 +1,93 @@
-setInterval(function () {
-  // Zurich
-  let zurichElement = document.querySelector("#zurich");
-  let zurichDateElement = zurichElement.querySelector(".date");
-  let zurichTimeElement = zurichElement.querySelector(".time");
-  let zurichZone = moment().tz("Europe/Zurich");
+let clockIntervalId;
+let clockIntervalTime;
+function updateTimeCities() {
+  clockIntervalId = setInterval(function () {
+    // Zurich
+    let zurichElement = document.querySelector("#zurich");
+    let zurichDateElement = zurichElement.querySelector(".date");
+    let zurichTimeElement = zurichElement.querySelector(".time");
+    let zurichZone = moment().tz("Europe/Zurich");
 
-  zurichTimeElement.innerHTML = `${zurichZone.format("hh:mm:ss")} <small>${zurichZone.format("A")}</small>`
-  zurichDateElement.innerHTML = zurichZone.format("dddd, MMMM Do YYYY");
+    zurichTimeElement.innerHTML = `${zurichZone.format("hh:mm:ss")} <small>${zurichZone.format("A")}</small>`
+    zurichDateElement.innerHTML = zurichZone.format("dddd, MMMM Do YYYY");
 
-  // London
-  let londonElement = document.querySelector("#london");
-  let londonTimeElement = londonElement.querySelector(".time");
-  let londonZone = moment().tz("Europe/London");
+    // London
+    let londonElement = document.querySelector("#london");
+    let londonTimeElement = londonElement.querySelector(".time");
+    let londonZone = moment().tz("Europe/London");
 
-  londonTimeElement.innerHTML = `${londonZone.format("hh:mm:ss")} <small>${londonZone.format("A")}</small>`
+    londonTimeElement.innerHTML = `${londonZone.format("hh:mm:ss")} <small>${londonZone.format("A")}</small>`
 
-  // Sydney
-  let sydneyElement = document.querySelector("#sydney");
-  let sydneyTimeElement = sydneyElement.querySelector(".time");
-  let sydneyZone = moment().tz("Australia/Sydney");
+    // Sydney
+    let sydneyElement = document.querySelector("#sydney");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+    let sydneyZone = moment().tz("Australia/Sydney");
 
-  sydneyTimeElement.innerHTML = `${sydneyZone.format("hh:mm:ss")} <small>${sydneyZone.format("A")}</small>`
+    sydneyTimeElement.innerHTML = `${sydneyZone.format("hh:mm:ss")} <small>${sydneyZone.format("A")}</small>`
 
-  // New York
-  let newYorkElement = document.querySelector("#new-york");
-  let newYorkTimeElement = newYorkElement.querySelector(".time");
-  let newYorkZone = moment().tz("America/New_York");
+    // New York
+    let newYorkElement = document.querySelector("#new-york");
+    let newYorkTimeElement = newYorkElement.querySelector(".time");
+    let newYorkZone = moment().tz("America/New_York");
 
-  newYorkTimeElement.innerHTML = `${newYorkZone.format("hh:mm:ss")} <small>${newYorkZone.format("A")}</small>`
-}, 1000)
+    newYorkTimeElement.innerHTML = `${newYorkZone.format("hh:mm:ss")} <small>${newYorkZone.format("A")}</small>`
+  }, 1000)
+};
+
+// Clock 
+function updateClock() {
+  clockIntervalTime = setInterval(function () {
+    let now = moment();
+    let hour = now.hours();
+    let minute = now.minutes();
+    let second = now.seconds();
+
+    let handHour = document.querySelector('.hour-hand');
+    let hourDegrees = ((hour % 12) / 12) * 360 + (minute / 60) * 30 + 360;
+    handHour.style.transform = `translateX(-50%) rotate(${hourDegrees}deg)`;
+
+    let secondHand = document.querySelector('.second-hand')
+    let secondDegrees = (second / 60) * 360 + 360;
+    secondHand.style.transform = `translateX(-50%) rotate(${secondDegrees}deg)`;
+
+    let minuteHand = document.querySelector('.minute-hand')
+    let minuteDegrees = (minute / 60) * 360 + 363;
+    minuteHand.style.transform = `translateX(-50%) rotate(${minuteDegrees}deg)`;
+    if (second === 59) {
+      secondHand.style.transition = 'none';
+    }
+  }, 1000)
+}
 
 
-let select = document.querySelector("#select");
-select.addEventListener("change", function (event) {
+function updateMainCity(event) {
+  clearInterval(clockIntervalId);
   let timeZone = event.target.value;
   if (timeZone === "current") {
     timeZone = moment.tz.guess();
   }
+  setInterval(function () {
+    let cityTime = moment().tz(timeZone);
+    let cityMainElement = document.querySelector("#zurich")
 
-  let cityTime = moment().tz(timeZone);
-  let cityMainElement = document.querySelector("#zurich")
+    let cityName = timeZone.split("/")[1];
 
-  let cityName = timeZone.split("/")[1];
+    cityMainElement.innerHTML = `
+        <div class="zurich">
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("dddd, MMMM Do YYYY")}</div>
+          <div class="time">${cityTime.format("hh:mm:ss")}<small>${cityTime.format("A")}</small></div>
+        </div>
+ `
+    console.log(timeZone)
+    console.log(cityTime.format("hh:mm:ss"))
+  }, 1000)
+};
 
 
-  cityMainElement.innerHTML = `<div class="main-city">
-        <h2>${cityName}</h2>
-        <div class="date">${cityTime.format("dddd, MMMM Do YYYY")}</div>
-        <div class="time">${cityTime.format("hh:mm:ss")}<small>${cityTime.format("A")}</small></div>
-      </div>`
-  console.log(cityTime)
-})
+let select = document.querySelector("#select");
+select.addEventListener("change", updateMainCity);
 
-// Clock
-function updateClock() {
-  let now = moment();
-  let hour = now.hours();
-  let minute = now.minutes();
-  let second = now.seconds();
-
-  let handHour = document.querySelector('.hour-hand');
-  let hourDegrees = ((hour % 12) / 12) * 360 + (minute / 60) * 30 + 360;
-  handHour.style.transform = `translateX(-50%) rotate(${hourDegrees}deg)`;
-
-  let secondHand = document.querySelector('.second-hand')
-  let secondDegrees = (second / 60) * 360 + 360;
-  secondHand.style.transform = `translateX(-50%) rotate(${secondDegrees}deg)`;
-
-  let minuteHand = document.querySelector('.minute-hand')
-  let minuteDegrees = (minute / 60) * 360 + 363;
-  minuteHand.style.transform = `translateX(-50%) rotate(${minuteDegrees}deg)`;
-  if (second === 59) {
-    // Disable transition to avoid the spin effect
-    secondHand.style.transition = 'none';
-  }
-
-}
-
-setInterval(updateClock, 1000);
-
+updateTimeCities();
 updateClock();
+
